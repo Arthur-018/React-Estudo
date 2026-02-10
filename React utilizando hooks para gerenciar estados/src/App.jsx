eea079c9e427e20ea8edd345fe2f1084255b12a7
Arthur-018 <toledoarthur11@gmail.com>
2026-02-10 11:14:15 -0300

    Aula alura REACT utilizando hooks

--- React utilizando hooks para gerenciar estados/src/App.jsx
+++ React utilizando hooks para gerenciar estados/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { use, useState } from "react"
 import { ChecklistsWrapper } from "./components/ChecklistsWrapper"
 import { Container } from "./components/Container"
 import { Dialog } from "./components/Dialog"
@@ -7,62 +7,27 @@ import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
 import { Heading } from "./components/Heading"
 import { IconPlus, IconSchool } from "./components/icons"
-import { SubHeading } from "./components/SubHeading"
 import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
 import { TextInput } from "./components/TextInput"
 import { Button } from "./components/Button"
 import { TodoForm } from "./components/ToDoForm"
-
-/* const todos = [
-  {
-    id: 1,
-    description: "JSX e componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 2,
-    description: "Props, state e hooks",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 3,
-    description: "Ciclo de vida dos componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 4,
-    description: "Testes unitários com Jest",
-    completed: false,
-    createdAt: "2022-10-31"
-  }
-]
-const completed = [
-  {
-    id: 5,
-    description: "Controle de inputs e formulários controlados",
-    completed: true,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 6,
-    description: "Rotas dinâmicas",
-    completed: true,
-    createdAt: "2022-10-31"
-  }
-] */
+import { TodoGroup } from "./components/TodoGroup"
+import TodoContext from "./components/TodoProvider/TodoContext"
 
 function App() {
 
   const [showDialog, setShowDialog] = useState(false)
+  const { todos, addTodo } = use(TodoContext)
 
   const toogleDialog = () => {
     setShowDialog(!showDialog)
   }
 
+  const handleFormSubmit = (formData) => {
+    addTodo(formData)
+    toogleDialog()
+  }
 
 
   return (
@@ -75,45 +40,19 @@ function App() {
         </Header>
         <ChecklistsWrapper>
 
-         { /* <TodoGroup
+          <TodoGroup
             heading="para estudar"
             items={todos.filter(t => !t.completed)}
-            onToggleCompleted={toggleTodoCompleted}
-            onDeleteTodo={deleteTodo}
           />
 
           <TodoGroup
             heading="Concluido"
-            items={todos.filter(t => !t.completed)}
-            onToggleCompleted={toggleTodoCompleted}
-            onDeleteTodo={deleteTodo}
-          /> */ }
-          
-         { /* <SubHeading>Para estudar</SubHeading>
-          <ToDoList>
-            {todos.filter(t => !t.completed).map(function (t) {
-              return <ToDoItem
-                key={t.id}
-                item={t}
-                onToggleCompleted={toggleTodoCompleted}
-                onDeleteTodo={deleteTodo}
-              />
-            })}
-          </ToDoList>
-          <SubHeading>Concluído</SubHeading>
-          <ToDoList>
-            {todos.filter(t => t.completed).map(function (t) {
-              return <ToDoItem
-                key={t.id}
-                item={t}
-                onToggleCompleted={toggleTodoCompleted}
-                onDeleteTodo={deleteTodo}
-              />
-            })}
-          </ToDoList> */}
+            items={todos.filter(t => t.completed)}
+          />
+
           <Footer>
             <Dialog isOpen={showDialog} onCLose={toogleDialog}>
-              {/*<TodoForm onSubmit={addTodo} />*/}
+              <TodoForm onSubmit={handleFormSubmit} />
             </Dialog>
             <FabButton onClick={toogleDialog}>
               <IconPlus />
