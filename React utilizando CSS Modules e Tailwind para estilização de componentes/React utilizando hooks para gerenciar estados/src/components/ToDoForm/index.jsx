26997776d517efe1da4591306215e8826d8d6343
Arthur-018 <toledoarthur11@gmail.com>
2026-02-12 08:44:37 -0300

    React utilizando CSS Modules e Tailwind

---
+++ React utilizando CSS Modules e Tailwind para estilização de componentes/React utilizando hooks para gerenciar estados/src/components/ToDoForm/index.jsx
@@ -0,0 +1,20 @@
+import { Button } from '../Button'
+import { TextInput } from '../TextInput'
+import './Todo-form.style.css'
+
+export function TodoForm({onSubmit, defaultValue}) {
+
+    return (
+        <form action={onSubmit} className='todo-form'>
+            <TextInput
+             placeholder="Digite o item que deseja adicionar" 
+            required
+            name="description"
+            defaultValue={defaultValue}
+            />
+            <Button>
+                Salvar item
+            </Button>
+        </form>
+    )
+}
